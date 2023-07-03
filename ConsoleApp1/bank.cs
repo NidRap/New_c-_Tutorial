a6cded1a38dc94a90693ac18b441918111ccab24
neddhe.raparia <neddhe.raparia@KELLGGNLPTP1482>
2023-07-03 15:17:26 +0530

    new commit 2

--- ConsoleApp1/bank.cs
+++ ConsoleApp1/bank.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace ConsoleApp1
 {
- 
+
     public class BankAccount
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get;
 
+        }
+
+
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
-            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            //
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+       
         }
-    }
+
+
 }
+    }
+
 
